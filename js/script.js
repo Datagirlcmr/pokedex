ecbf24b8638873fa99bbcd9cf6532e023875a2fb
Datagirl <datagirlcmr@gmail.com>
2022-08-22 23:04:47 +0100

    create a pokemon list and add populate with pokemon characters

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-alert('Hello World')
+let pokemonList = [];
+pokemonList[0] = {
+    name: 'Bulbusaur',
+    height: 1,
+    types: ['grass', 'poison']
+}
+pokemonList[1] = {
+    name: 'Charmander',
+    height: 1,
+    types: ['grass', 'poison']
+}
+pokemonList[2] = {
+    name: 'Ivysaur',
+    height: 1,
+    types: ['grass', 'poison']
+}
 
-let favoriteFood = 'eru';
-document.write(favoriteFood);
+console.log(pokemonList)
