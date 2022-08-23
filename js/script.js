2a1268df63420700d93019476e8457a8a00b432b
Datagirl <datagirlcmr@gmail.com>
2022-08-23 11:16:44 +0100

    add loops and conditional statements on pokedex characters

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 let pokemonList = [];
 pokemonList[0] = {
     name: 'Bulbusaur',
-    height: 1,
+    height: 0.7,
     types: ['grass', 'poison']
 }
 pokemonList[1] = {
     name: 'Charmander',
-    height: 1,
+    height: 1.3,
     types: ['grass', 'poison']
 }
 pokemonList[2] = {
     name: 'Ivysaur',
-    height: 1,
+    height: 0.3,
     types: ['grass', 'poison']
 }
+let i = 0
+while (i < pokemonList.length) {
 
-console.log(pokemonList)
+    if (pokemonList[i].height > 1) {
+        document.write(`${pokemonList[i].name} (Height: ${pokemonList[i].height}) Wow that's big <br>`)
+    }
+    else {
+        document.write(`${pokemonList[i].name} (Height: ${pokemonList[i].height}) <br>`)
+    }
+    i++
+}
+
+
