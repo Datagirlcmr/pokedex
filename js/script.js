1af865dd7cb1f8756c450234a1bc0d6182ad7ffa
Datagirl <datagirlcmr@gmail.com>
2022-09-22 15:54:18 +0100

    use foreach to iterate over the pokedex array

--- js/script.js
+++ js/script.js
@@ -1,29 +1,41 @@
 let pokemonList = [];
 pokemonList[0] = {
-    name: 'Bulbusaur',
-    height: 0.7,
-    types: ['grass', 'poison']
-}
+  name: "Bulbusaur",
+  height: 0.7,
+  types: ["grass", "poison"],
+};
 pokemonList[1] = {
-    name: 'Charmander',
-    height: 1.3,
-    types: ['grass', 'poison']
-}
+  name: "Charmander",
+  height: 1.3,
+  types: ["grass", "poison"],
+};
 pokemonList[2] = {
-    name: 'Ivysaur',
-    height: 0.3,
-    types: ['grass', 'poison']
-}
-let i = 0
-while (i < pokemonList.length) {
+  name: "Ivysaur",
+  height: 0.3,
+  types: ["grass", "poison"],
+};
 
-    if (pokemonList[i].height > 1) {
-        document.write(`${pokemonList[i].name} (Height: ${pokemonList[i].height}) Wow that's big <br>`)
-    }
-    else {
-        document.write(`${pokemonList[i].name} (Height: ${pokemonList[i].height}) <br>`)
-    }
-    i++
-}
+pokemonList.forEach((pokemon) => {
+  document.write(`${pokemon.name} (Height: ${pokemon.height}) <br>`);
+});
 
+// let pokemonRepository = (function () {
+//     let pokemonList = []; // empty array
 
+//     let add = function (pokemon) {
+//       pokemonList.push(pokemon);
+//     };
+
+//     let getAll = function () {
+//       return pokemonList;
+//     };
+
+//     return {
+//       add: add,
+//       getAll: getAll,
+//     };
+//   })();
+
+//   console.log(pokemonRepository.getAll()); // []
+//   pokemonRepository.add({ name: "Pikachu" });
+//   console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
